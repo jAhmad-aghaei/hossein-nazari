111783b6a7774940e161b74dc99f6ab608b5c66b
iihossein <1901zenith1901@gmail.com>
2024-11-11 11:15:29 +0330

    code is edited

--- cal.js
+++ cal.js
@@ -9,7 +9,7 @@ function calculator(num1, operator, num2) {
             result = num1 - num2;
             break;
         case '*':
-            result = num1 * num2;
+            result = num1 + num2;
             break;
         case '/':
             result = num2 !== 0 ? num1 / num2 : 'Cannot divide by zero';
